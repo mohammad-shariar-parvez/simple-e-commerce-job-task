191727580bf1698c487d0360d9ea1aed764472e6
Piaowei <msptonmoy@gmail.com>
2024-02-13 03:30:04 +0600

    update(pages): refactored codes,removed console and comments

--- src/components/layouts/skeleton/ProductDetailsScalation.jsx
+++ src/components/layouts/skeleton/ProductDetailsScalation.jsx
@@ -4,7 +4,7 @@ const ProductDetailsSkeleton = () => {
   return (
     <section className='bg-[#EDEDED] animate-pulse '>
       <div className='pb-16'></div>
-      <div className='container bg-white mt-10 md:p-10 rounded-lg mb-12'>
+      <div className='max-w-7xl mx-auto p-4 bg-white mt-10 md:p-10 rounded-lg mb-12'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
           <div>
             {/* Placeholder for image skeleton */}
