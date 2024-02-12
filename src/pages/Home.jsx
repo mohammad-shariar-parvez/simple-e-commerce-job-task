80d2b49585cdda60f5d13e7df7710bd930a13f0c
Piaowei <msptonmoy@gmail.com>
2024-02-13 03:29:43 +0600

    update(pages): refactored codes,removed console and comments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,15 @@
 import Navbar from '../components/layouts/ui/Navbar';
 import Hero from '../components/layouts/ui/Hero';
 import ProductCatalog from '../components/layouts/ui/ProductCatalog';
+import ProductSearchBar from '../components/layouts/ui/ProductSearchBar';
 
 const Home = () => {
-  // console.log('Response Data:', products);
   return (
     <div className='m-0 p-4 box-border font-poppins  relative text-gray-600 '>
       <Navbar />
       <Hero />
+      <ProductSearchBar />
       <ProductCatalog />
-      {/* <!-- CARD --> */}
-      {/* <section className='mb-8 md:max-w-4xl mx-auto '>
-        <div className='sidebar'></div>
-      </section> */}
-
-      {/* <!--######################################### 
-					    SIdebar
-	    #########################################--> */}
-      {/* <section
-        id='cartSection'
-        className='fixed right-0 top-0  w-80 h-full bg-tomato  z-20 transition-transform transform translate-x-full ease-in-out duration-500  '
-      >
-        <div className='text-tomato flex items-center justify-between p-3 bg-white text-sm '>
-          <div className='text-tomato space-x-2 font-medium '>
-            <i className='text-xl fa-sharp fa-solid fa-bag-shopping'></i>
-            <span id='count2'>0</span>
-            <span>Item</span>
-          </div>
-          <button
-            id='closeCartBtn'
-            className=' ml-auto  ring-1 ring-tomato ring-inse p-[2px] rounded-sm leading-none hover:ring-tomatoDark hover:ring-2'
-          >
-            Close
-          </button>
-        </div>
-        <div id='cartItem' className='p-3 text-white space-y-4'></div>
-        <div className='foot bg-white text-tomato font-medium text-center p-4 absolute bottom-0 w-full'>
-          <span>Place Order</span>
-          <span id='total'> 0</span>
-          <span>$</span>
-        </div>
-      </section> */}
     </div>
   );
 };
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useProductsQuery } from '../redux/api/productApi';
 import ProductDetailsSkeleton from '../components/layouts/skeleton/ProductDetailsScalation';
+import StarRating from '../components/layouts/ui/StarRating';
 const ProductDetails = () => {
   const { platform, item_id } = useParams();
 
@@ -14,34 +15,34 @@ const ProductDetails = () => {
     // Scroll to the top when component is mounted
     window.scrollTo(0, 0);
   }, []);
+
   if (isLoading && !isSuccess) {
     return <ProductDetailsSkeleton />;
   }
-  // console.log(data);
-  // console.log('params', platform, item_id);
 
   return (
     <section className='bg-[#EDEDED] '>
       <div className='pb-16'></div>
-      <div className='max-w-6xl mx-auto p-4  bg-white    mt-10 md:p-10   rounded-lg mb-12  '>
+      <div className='max-w-7xl mx-auto p-4  bg-white    mt-10 md:p-10   rounded-lg mb-12  '>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
           <div>
             <img
-              src={data?.image_urls[0]}
-              alt={data?.title}
+              src={data.image_urls[0]}
+              alt={data.title}
               className='w-full h-auto '
             />
           </div>
 
           <div>
             <h1 className='text-3xl font-semibold mb-4'>{data?.title}</h1>
-            <p className='text-gray-600 mb-4'>{data?.headline}</p>
+            <p className='text-gray-600 mb-4'>{data.headline}</p>
             <hr className=' h-1 mx-auto my-4 bg-gray-200 border-0 rounded md:my-10 ' />
+
             <div className='bg-[#f6f6f6] p-4 rounded mb-4'>
               <div className='flex items-center justify-between mb-4'>
                 <p className='text-lg font-bold text-tomato '>
                   <i className='fa-solid fa-yen-sign me-1'></i>
-                  {data?.currency} {data?.price}
+                  {data.currency} {data.price}
                 </p>
                 <p className='bg-red-100 text-tomato text-sm font-medium me-2 px-2.5 py-0.5 rounded  '>
                   {data?.brand}
@@ -49,23 +50,17 @@ const ProductDetails = () => {
 
                 <p>
                   <i className='fa-solid fa-cubes-stacked text-green-500 me-3'></i>
-                  {data?.availability ? 'In Stock' : 'Out of Stock'}
+                  {data.availability ? 'In Stock' : 'Out of Stock'}
                 </p>
               </div>
-              <p>
-                <i className='fa-solid fa-star text-yellow-400'></i>
-                <i className='fa-solid fa-star text-yellow-400'></i>
-                <i className='fa-solid fa-star text-yellow-400'></i>
-                <i className='fa-solid fa-star text-yellow-400'></i>
-                <i className='fa-solid fa-star text-yellow-400'></i>
-                <span className='ps-4 font-semibold me-1'>
-                  {data?.review_average}
-                </span>
-                <span>({data?.review_count} reviews)</span>
-              </p>
+              <StarRating
+                rating={data.review_average}
+                review_count={data.review_count}
+              />
             </div>
+
             <a
-              href={data?.item_url}
+              href={data.item_url}
               target='_blank'
               rel='noopener noreferrer'
               className='text-blue-500 hover:underline '
@@ -75,44 +70,47 @@ const ProductDetails = () => {
           </div>
         </div>
 
+        {/* Product Details */}
         <div className='mt-8'>
-          <p className='text-gray-700 mb-6'>{data?.description}</p>
+          <p className='text-gray-700 mb-6'>{data.description}</p>
           <h2 className='text-2xl font-semibold mb-4'>Product Details</h2>
           <ul className=' list-inside list-none space-y-1'>
             <li>
-              <strong>Brand:</strong> {data?.brand}
+              <strong>Brand:</strong> {data.brand}
             </li>
             <li>
               <strong>Availability:</strong>{' '}
-              {data?.availability ? 'In Stock' : 'Out of Stock'}
+              {data.availability ? 'In Stock' : 'Out of Stock'}
             </li>
             <li>
-              <strong>Shop:</strong> {data?.shop_name}
+              <strong>Shop:</strong> {data.shop_name}
             </li>
-            {/* Add more details as needed */}
           </ul>
         </div>
         <hr className=' border-2 mx-auto my-4 bg-gray-200  rounded md:my-10 ' />
+
+        {/* Additional Information */}
         <div className='mt-8'>
           <h2 className='text-2xl font-semibold mb-4'>
             Additional Information
           </h2>
 
           <p>
-            <strong>Genre:</strong> {data?.genre_name}
+            <strong>Genre:</strong> {data.genre_name}
           </p>
-          {/* Add more information as needed */}
         </div>
+
+        {/* Shop Reviews */}
         <hr className=' border-2 mx-auto my-4 bg-gray-200  rounded md:my-10 ' />
         <div className='mt-8'>
           <h2 className='text-2xl font-semibold mb-4 '>Shop Reviews</h2>
           <div className='space-y-1'>
             <p>
-              <strong>Shop Name:</strong> {data?.shop_name}
+              <strong>Shop Name:</strong> {data.shop_name}
             </p>
             <p>
-              <strong>Shop Average Rating:</strong> {data?.shop_review_average}{' '}
-              ({data?.shop_review_count} reviews)
+              <strong>Shop Average Rating:</strong> {data.shop_review_average} (
+              {data.shop_review_count} reviews)
             </p>
           </div>
         </div>
@@ -123,18 +121,3 @@ const ProductDetails = () => {
 };
 
 export default ProductDetails;
-
-// <div className=' '>
-//   <div className='md:flex   items-start md:space-x-4 space-y-4 md:space-y-0'>
-//     {/* <img
-//           src={data?.imageUrl}
-
-//           alt='eagle_image'
-//           className='w-full h-auto md:h-[290] md:w-auto '
-//         />
-
-//         <p className='text-base font-normal text-[#212529] text-justify'>
-//           {parsedContent}
-//         </p> */}
-//   </div>
-// </div>;
