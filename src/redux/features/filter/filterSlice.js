83c69d5c711e95253223fb3eb2e573dc3d1ab27f
Piaowei <msptonmoy@gmail.com>
2024-02-13 03:33:16 +0600

    update(actions): filtering and sorting and store products

--- src/redux/features/filter/filterSlice.js
+++ src/redux/features/filter/filterSlice.js
@@ -1,45 +1,78 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice } from '@reduxjs/toolkit';
+import { filterByPrice } from '../../../utils/filterByPrice.js';
+import { sortByFileds } from '../../../utils/sortByFileds.js';
 
 const initialState = {
-	fixedPrice: {
+	productPrice: {
 		minPrice: 0,
-		maxPrice: 9000,
+		maxPrice: Infinity,
 	},
-
-	searchPrice: {
-		minPrice: null,
-		maxPrice: null,
-	},
-
 	reviewCount: null,
 	reviewAverage: null,
 	sortBy: 'price',
 	sortOrder: 'desc',
+	searchTerm: 'shirt',
+	totalProducts: [],
+	filteredProducts: [],
+	slider: false
 };
 
 const filterSlice = createSlice({
 	name: 'filter',
 	initialState,
 	reducers: {
-		updateFixedPrice: (state, action) => {
-			// const { maxPrice, minPrice } = action.payload;
-			state.fixedPrice = action.payload;
+		updateProducts: (state, action) => {
+			state.totalProducts = action.payload;
+			//Don't use method chainnig here 
+			const filteredProducts = action.payload.filter((product) =>
+				filterByPrice(product, state?.productPrice)
+			);
+			const sortedProducts = filteredProducts.sort((a, b) =>
+				sortByFileds(a, b, state.sortBy, state.sortOrder)
+			);
+			state.filteredProducts = sortedProducts;
 		},
-		setSearchPrice: (state, action) => {
-			state.searchPrice = action.payload;
+
+		updateProductPrice: (state, action) => {
+			//filter in total products
+			state.productPrice = action.payload;
+			state.filteredProducts = state.totalProducts.filter((product) =>
+				filterByPrice(product, action.payload)
+			);
 		},
 
 		updateSortOption: (state, action) => {
+			//Sort on filtered products
 			const { sortBy, sortOrder } = action.payload;
 			state.sortBy = sortBy;
 			state.sortOrder = sortOrder;
+			state.filteredProducts = state.filteredProducts.sort((a, b) =>
+				sortByFileds(a, b, state.sortBy, state.sortOrder)
+			);
 		},
 
+		updateSearchTerm: (state, action) => {
+			const { searchQuery } = action.payload;
+			state.searchTerm = searchQuery;
+		},
 
+		resetSearchTerm: (state) => {
+			state.searchTerm = 'shirt';
+		},
+
+		sliderChange: (state) => {
+			state.slider = !state.slider;
+		},
 	},
 });
 
-export const { updateFixedPrice, setSearchPrice, updateSortOption } = filterSlice.actions;
+export const {
+	updateProductPrice,
+	updateSortOption,
+	updateSearchTerm,
+	resetSearchTerm,
+	updateProducts,
+	sliderChange
+} = filterSlice.actions;
 
 export default filterSlice.reducer;
